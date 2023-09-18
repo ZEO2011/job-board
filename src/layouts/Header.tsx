3e2478e89cbd3f3d8ee368e6f28ee4ef298c52cb
ziad hatem <ziadhatem384@gmail.com>
2023-09-18 18:53:58 +0300

    fixed phone navlinks problem

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,6 @@
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Dialog from "../components/Dialog"
 import { Link } from "react-router-dom"
 import { faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons"
@@ -11,6 +11,7 @@ export default function Header() {
 	const [dark, setDark] = useState(defaultTheme)
 	const [profileStatus, setProfileStatus] = useState(false)
 	const [phoneNavlinksStatus, setPhoneNavlinksStatus] = useState(false)
+	const phoneNavlinksRef = useRef<HTMLLIElement>(null)
 	const html = document.querySelector("html")
 	html?.classList.add(dark ? "dark" : "light")
 	html?.classList.remove(!dark ? "dark" : "light")
@@ -38,12 +39,17 @@ export default function Header() {
 		}
 		setProfileStatus((c) => !c)
 	}
-	function togglePhoneNavLinks() {
+	function togglePhoneNavLinks(e: React.MouseEvent) {
 		if (themeStatus) {
 			setThemeStatus(false)
 		}
-		setProfileStatus(false)
-		setPhoneNavlinksStatus((c) => !c)
+		if (e.currentTarget.classList.contains("hide")) {
+			setPhoneNavlinksStatus(false)
+			e.currentTarget.classList.remove("hide")
+		} else setPhoneNavlinksStatus(true)
+	}
+	function hidePhoneNavlinks() {
+		phoneNavlinksRef.current?.classList.add("hide")
 	}
 	return (
 		<>
@@ -92,31 +98,48 @@ export default function Header() {
 							</button>
 						</li>
 						<li
-							className="w-fit h-fit main-btn relative md:hidden block"
+							className="menu w-fit h-fit main-btn relative md:hidden block"
 							onClick={togglePhoneNavLinks}
+							ref={phoneNavlinksRef}
 						>
 							<button>
 								<FontAwesomeIcon icon={faBars} />
 								{phoneNavlinksStatus && (
-									<ul className="flex text-sm absolute right-36 top-0  h-full w-fit items-end justify-center flex-col">
+									<ul className="flex text-sm relative right-36 top-0  h-full w-fit items-end justify-center flex-col">
 										<Dialog className="w-fit h-fit">
-											<li className=" h-fit main-btn w-full text-start">
-												<Link to="/tasks">
+											<li
+												className=" h-fit main-btn w-full text-start"
+												onClick={
+													hidePhoneNavlinks
+												}
+											>
+												<Link
+													className="w-full h-full"
+													to="/tasks"
+												>
 													task board
 												</Link>
 											</li>
-											<li className=" h-fit main-btn w-full text-start">
-												<Link to="/jobs">
+											<li
+												className=" h-fit main-btn w-full text-start"
+												onClick={
+													hidePhoneNavlinks
+												}
+											>
+												<Link
+													className="w-full h-full"
+													to="/jobs"
+												>
 													job listing
 												</Link>
 											</li>
 											<li
 												className=" h-fit main-btn w-full text-start user relative"
-												onMouseOver={() =>
+												onClick={() => {
 													setProfileStatus(
-														true,
+														(c) => !c,
 													)
-												}
+												}}
 											>
 												<button className="flex gap-2 items-center justify-between py-1">
 													hatemziad384@gmail.com
@@ -131,12 +154,22 @@ export default function Header() {
 													/>
 												</button>
 												{profileStatus && (
-													<Dialog className="left-[40%] w-32">
-														<button className="main-btn text-start pl-2 py-2">
+													<Dialog className="left-0 w-32">
+														<button
+															className="main-btn text-start pl-2 py-2"
+															onClick={() => {
+																hidePhoneNavlinks()
+															}}
+														>
 															my
 															listing
 														</button>
-														<button className="main-btn text-start pl-2 py-2">
+														<button
+															className="main-btn text-start pl-2 py-2"
+															onClick={() => {
+																hidePhoneNavlinks()
+															}}
+														>
 															logout
 														</button>
 													</Dialog>
@@ -150,10 +183,14 @@ export default function Header() {
 					</ul>
 					<ul className="gap-2 text-lg h-full w-fit items-center justify-center md:flex hidden">
 						<li className="w-fit h-fit main-btn">
-							<Link to="/tasks">task board</Link>
+							<Link className="w-full h-full" to="/tasks">
+								task board
+							</Link>
 						</li>
 						<li className="w-fit h-fit main-btn">
-							<Link to="/jobs">job listing</Link>
+							<Link className="w-full h-full" to="/jobs">
+								job listing
+							</Link>
 						</li>
 						<li
 							className="w-fit h-fit main-btn user relative"
