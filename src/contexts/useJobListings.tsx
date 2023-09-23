da8bfe841d2dffc1ff9ee79d0abb66440f449e45
ziad hatem <ziadhatem384@gmail.com>
2023-09-23 21:59:09 +0300

    run npm run build

--- src/contexts/useJobListings.tsx
+++ src/contexts/useJobListings.tsx
@@ -3,12 +3,10 @@ import { JobListingsContext } from "../layouts/RootLayout"
 
 export default function useJobListings() {
 	const value = useContext(JobListingsContext)
-
 	if (value === null || value === undefined) {
 		throw new Error(
 			"useJobListings must be used within a JobListingsProvider",
 		)
 	}
-
 	return value
 }
